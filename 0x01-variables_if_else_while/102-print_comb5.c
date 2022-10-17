0fda1abc88c94ae283d45f9f9daa6d49bb2adf2d
isaac okyere <isaacookyere4@gmail.com>
2022-10-17 13:44:35 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,30 +12,42 @@ int main(void)
 {
 	int a, b, c, d;
 
-	a = b = c = 48; d = 49;
+	a = b = c = 48;
+       	d = 49;
 	while  ((a < 58))
 	{
-		putchar(a); putchar(b); putchar(32); putchar(c); putchar(d);
+		putchar(a);
+	       	putchar(b);
+	       	putchar(32);
+	       	putchar(c);
+	       	putchar(d);
 		if ((a == 57) && (b == 56) && (c == 57) && (d == 57))
 		{
-			putchar('\n'); a++;
+			putchar('\n'); 
+			a++;
 		}
 		else
 		{
-			putchar(44); putchar(32);
+			putchar(44); 
+			putchar(32);
 			if ((c == 57) && (d == 57))
 			{
 				if (b < 56)
 				{
-					d = ++b + 1; c = a;
+					d = ++b + 1;
+				       	c = a;
 				}
 				else if (b == 56)
 				{
-					b++; c = a + 1; d = 48;
+					b++; 
+					c = a + 1;
+				       	d = 48;
 				}
 				else if (b == 57)
 				{
-					b = 48; d = 49; c = ++a;
+					b = 48; 
+					d = 49;
+				       	c = ++a;
 				}
 			}
 			else if (d < 57)
@@ -44,7 +56,8 @@ int main(void)
 			}
 			else
 			{
-				d = 48; c++;
+				d = 48;
+			       	c++;
 			}
 		}
 	}
