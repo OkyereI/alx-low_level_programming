765ce66a5c4ae5edbaae9aa570e54cd9f8b71955
isaac okyere <isaacookyere4@gmail.com>
2022-10-24 13:04:20 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string .
+ *  * @str: string.
+ * Return: String length
+*/
+ void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
