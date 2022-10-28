fc82580cfc1560dd01d71ff44c574adc4d6b92f1
isaac okyere <isaacookyere4@gmail.com>
2022-10-28 08:41:13 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *   print_alphabet_x10 - entry point
- *     Description: Prints the alphabet with _putchar ten times
- *  Return: void
- */ 
+ * print_alphabet_x10 - entry point
+ * Description: Prints the alphabet with _putchar ten times
+ * Return: void
+ */
 
 void print_alphabet_x10(void)
 {
-		char x;
-			int i;
+	char x;
+	int i;
 
-				for (i = 0; i < 10; i++)
-					{
-					for (x = 'a'; x <= 'z'; x++)
-						{
-																						_putchar(c);
-																								}
-				_putchar('\n');
-				}
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
 }
