886fbf43acb27e440393419aa8f5ad14064b2b84
isaac okyere <isaacookyere4@gmail.com>
2022-10-30 05:16:47 -0700

    finabacci 4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long fib1_half1, fib1_half2, fib_half1, fib2_half2;
 	unsigned long half1, half2;
 
 	for (count = 0; count < 92; count++)
@@ -22,13 +22,13 @@ int main(void)
 		fib2 = sum;
 	}
 	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
+	fib_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
 
 	for (count = 93; count < 99; count++)
 	{
-		half1 = fib1_half1 + fib2_half1;
+		half1 = fib1_half1 + fib_half1;
 		half2 = fib1_half2 + fib2_half2;
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
@@ -39,8 +39,9 @@ int main(void)
 		if (count != 98)
 			printf(", ");
 
-		fib1_half1 = fib2_half1;							fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
+		fib1_half1 = fib_half1;
+		fib1_half2 = fib2_half2;
+		fib_half1 = half1;
 		fib2_half2 = half2;
 	}
 
