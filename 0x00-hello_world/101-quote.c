e4cfd642ce6670523064b0a898dfdc72c970111f
Isaac Okyere <isaacokyere4@gmail.com>
2023-02-16 03:25:43 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 
 /**
- *   main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *     followed by a new line, to standard error.
  *     Return: Always 1.
  */
@@ -16,5 +16,5 @@ int main(void)
 				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 				59);
 
-			return (1);
+	return (1);
 }
