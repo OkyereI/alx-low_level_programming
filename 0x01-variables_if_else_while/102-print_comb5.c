c5f2645781f3b742f1acf2df714d74b0ccde5e7f
Isaac Okyere <isaacokyere4@gmail.com>
2023-02-17 06:07:32 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two different digits,
- *in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
- */
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-	int digit1, digit2;
+	int c, i, k, j;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (c = 48; c <= 57; c++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		for (i = 48; i <= 57; i++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
 
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
 		}
 	}
 
