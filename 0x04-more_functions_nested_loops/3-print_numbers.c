2584b8201d60593aabf7ab7181afbb6d66eebf36
Isaac Okyere <isaacokyere4@gmail.com>
2023-03-07 14:12:05 -0800

    3-print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_numbers - Print numbers between 0 to 9 incl.
- *
- * Return: Void.
- */
+*print_numbers - Print numbers between 0 to 9 incl.
+*
+*Return:Void.
+*/
 void print_numbers(void)
 {
 	int i;
