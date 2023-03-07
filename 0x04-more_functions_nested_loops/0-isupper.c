87507782ceceae54666c8aed49ae8985b5ad109b
Isaac Okyere <isaacokyere4@gmail.com>
2023-03-07 13:55:18 -0800

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * _isupper - Check if paramter c represents an uppercase letter.
- * @c: Int representing a character
- * Return: Always 0.
- */
+*_isupper - Check if paramter c represents an uppercase letter.
+*@c: Int representing a character
+*Return: Always 0.
+*/
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
