218d3145e594127a302e30a1bda77a9afa7f8ba0
isaac okyere <122814014+OkyereI@users.noreply.github.com>
2023-12-13 23:31:56 +0000

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,64 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * rec_search - searches for a value in an array of
- * integers using the Binary search algorithm
+ * main - Entry point
  *
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * Return: Always EXIT_SUCCESS
  */
-int rec_search(int *array, size_t size, int value)
+int main(void)
 {
-	size_t half = size / 2;
-	size_t i;
+    int array[] = {
+        0, 1, 2, 5, 5, 6, 6, 7, 8, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
 
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-	{
-		if (half > 0)
-			return (rec_search(array, half + 1, value));
-		return ((int)half);
-	}
-
-	if (value < array[half])
-		return (rec_search(array, half + 1, value));
-
-	half++;
-	return (rec_search(array + half, size - half, value) + half);
-}
-
-/**
- * advanced_binary - calls to rec_search to return
- * the index of the number
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
-int advanced_binary(int *array, size_t size, int value)
-{
-	int index;
-
-	index = rec_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
-		return (-1);
-
-	return (index);
+    printf("Found %d at index: %d\n\n", 8, advanced_binary(array, size, 8));
+    printf("Found %d at index: %d\n\n", 5, advanced_binary(array, size, 5));
+    printf("Found %d at index: %d\n\n", 999, advanced_binary(array, size, 999));
+    printf("Found %d at index: %d\n\n", 2, advanced_binary(array, size, 2));
+    printf("Found %d at index: %d\n\n", 6, advanced_binary(array, size, 6));
+    printf("Found %d at index: %d\n\n", 1, advanced_binary(array, size, 1));
+    return (EXIT_SUCCESS);
 }
